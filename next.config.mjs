23769dee00897a433af6eef386358bccd9adb184
Maxim <Buldov@mail.ru>
2024-09-12 13:55:06 -0700

    setup pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+};
+
+export default withPWA({
+	dest: 'public',
+	register: true,
+	skipWaiting: true,
+})(nextConfig);
