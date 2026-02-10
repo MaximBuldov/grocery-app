c529575e82aa2733c974eb2ee10cdaa305361a70
Maksim Buldau <maxbuldov@gmail.com>
2026-02-10 14:50:47 -0800

    setup deploy

--- src/utils/get-query-client.ts
+++ src/utils/get-query-client.ts
@@ -1,43 +1,43 @@
 import {
-	MutationCache,
-	QueryCache,
-	QueryClient,
-	defaultShouldDehydrateQuery,
-	isServer,
-} from '@tanstack/react-query';
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  defaultShouldDehydrateQuery,
+  isServer,
+} from "@tanstack/react-query";
 
 function makeQueryClient() {
-	return new QueryClient({
-		defaultOptions: {
-			queries: {
-				staleTime: 60 * 1000,
-			},
-			dehydrate: {
-				shouldDehydrateQuery: (query) =>
-					defaultShouldDehydrateQuery(query) ||
-					query.state.status === 'pending',
-			},
-		},
-		queryCache: new QueryCache({
-			onError: () => {
-				!isServer && window.alert('Something went wrong!');
-			},
-		}),
-		mutationCache: new MutationCache({
-			onError: () => {
-				!isServer && window.alert('Something went wrong!');
-			},
-		}),
-	});
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+      dehydrate: {
+        shouldDehydrateQuery: (query) =>
+          defaultShouldDehydrateQuery(query) ||
+          query.state.status === "pending",
+      },
+    },
+    queryCache: new QueryCache({
+      onError: () => {
+        !isServer && window.alert("Something went wrong!");
+      },
+    }),
+    mutationCache: new MutationCache({
+      onError: () => {
+        !isServer && window.alert("Something went wrong!");
+      },
+    }),
+  });
 }
 
 let browserQueryClient: QueryClient | undefined = undefined;
 
 export function getQueryClient() {
-	if (isServer) {
-		return makeQueryClient();
-	} else {
-		if (!browserQueryClient) browserQueryClient = makeQueryClient();
-		return browserQueryClient;
-	}
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
 }
